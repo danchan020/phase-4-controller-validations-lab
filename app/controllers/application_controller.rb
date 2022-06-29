94e645b8708e1a5e160a0f19c25b13615896bc44
Daniel <danchan020@gmail.com>
2022-06-29 16:21:41 -0700

    commit tree cf0fe3209d64653b18babd268c5da088cc5269b1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+    private
+
+    def invalid_record(exception)
+        render json: {errors: exception.record.errors}, status: :unprocessable_entity
+    end
 end
